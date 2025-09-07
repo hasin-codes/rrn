faae94ed88adef355676bb235447d9a44bfd5679
Hasin Raiyan <fullcopyrightsounds@gmail.com>
2025-09-08 02:58:35 +0600

    speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import localFont from 'next/font/local'
 import './globals.css'
 import { Navbar } from '@/components/Navbar'
 import { Footer } from '@/components/Footer'
-import { Analytics } from "@vercel/analytics/next
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const texGyreAdventor = localFont({
   src: '../public/fonts/texgyreadventor-bold.otf',
@@ -108,6 +109,7 @@ export default function RootLayout({
           </div>
         </div>
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   )
